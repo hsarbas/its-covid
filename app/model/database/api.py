b62d343819f0b1d79429b188bfce163d338bee05
Edgar-Ramos-II <edgarramos820@gmail.com>
2020-04-01 15:00:26 +0800

    PH_masterlis api with offset and limit

--- app/model/database/api.py
+++ app/model/database/api.py
@@ -5,8 +5,10 @@ import csv
 import json
 import requests
 
+
 class Database(object):
     """ Api database"""
+
     def __init__(self):
         self.connect()
 
@@ -15,10 +17,16 @@ class Database(object):
         Connect to 'covid-19' database
         :return:
         """
+
+        limit = 2000
+        offset = 0
         url = "https://services5.arcgis.com/mnYJ21GiFTR97WFg/arcgis/rest/services/PH_masterlist/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=%2A&outSR=102100&cacheHint=true&fbclid=IwAR3WIYausDM0uo0Hk8AIjuwk60hJe0ay60Z7utrGHRNyXtQGvBKx8BeoZXI"
-        response = requests.request("GET", url)
-        data =response.content
+        data = dict(resultOffset=offset ,resultRecordCount=limit)
+        response = requests.request("GET", url, params=data)
+        data = response.content
         self.PH_masterlist = json.loads(data)
+
+
 
     def get_all_records(self):
         """
